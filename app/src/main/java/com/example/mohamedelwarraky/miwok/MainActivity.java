be4ef2540734e0330bf85e06c718cf28344bb785
MohamedElwarraky <ModyElwarraky@gmail.com>
2018-07-20 04:20:56 +0200

    make ViewPager and display categories in tabs

--- app/src/main/java/com/example/mohamedelwarraky/miwok/MainActivity.java
+++ app/src/main/java/com/example/mohamedelwarraky/miwok/MainActivity.java
@@ -1,16 +1,19 @@
 package com.example.mohamedelwarraky.miwok;
 
 
-import android.content.Intent;
+import android.content.Context;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+/**
+ * Displays a {@link ViewPager} where each page shows a different day of the week.
+ */
 
+public class MainActivity extends AppCompatActivity {
 
     /*
     The reference value of time
@@ -21,83 +24,48 @@ public class MainActivity extends AppCompatActivity {
      */
     Toast mToast;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
 
+        // Find the view pager that will allow the user to swipe between fragments
+        ViewPager viewPager = findViewById(R.id.viewpager);
 
-        //find the view that show the numbers category
-        TextView numbers = findViewById(R.id.numbers);
-        // set onClickListener on that view
-        numbers.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        // Create an adapter that knows which fragment should be shown on each page
+        SimpleFragmentPagerAdapter mPagerAdapter = new SimpleFragmentPagerAdapter(this ,getSupportFragmentManager());
 
-                // create a new intent to open the {@link NumbersActivity}
-                Intent myintent = new Intent(MainActivity.this, NumbersActivity.class);
-                // start the new activity
-                startActivity(myintent);
+        // Set the adapter onto the view pager
+        viewPager.setAdapter(mPagerAdapter);
 
-            }
-        });
+        // Find the tab layout that shows the tabs
+        TabLayout tabLayout = findViewById(R.id.tabs);
 
-        //find the view that show the family category
-        TextView family = findViewById(R.id.family);
-        // set onClickListener on that view
-        family.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // create a new intent to open the {@link FamilyMembersActivity}
-                Intent myintent = new Intent(MainActivity.this, FamilyMembersActivity.class);
-                // start the new activity
-                startActivity(myintent);
-            }
-        });
-
-
-        //find the view that show the colors category
-        TextView color = findViewById(R.id.colors);
-        // set onClickListener on that view
-        color.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // create a new intent to open the {@link ColorsActivity}
-                Intent myintent = new Intent(MainActivity.this, ColorsActivity.class);
-                // start the new activity
-                startActivity(myintent);
-            }
-        });
-
-        //find the view that show the phrases category
-        TextView phrases = findViewById(R.id.phrases);
-        // set onClickListener on that view
-        phrases.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // create a new intent to open the {@link PhrasesActivity}
-                Intent myintent = new Intent(MainActivity.this, PhrasesActivity.class);
-                // start the new activity
-                startActivity(myintent);
-            }
-        });
-
+        // Connect the tab layout with the view pager. This will
+        //   1. Update the tab layout when the view pager is swiped
+        //   2. Update the view pager when a tab is selected
+        //   3. Set the tab layout's tab names with the view pager's adapter's titles
+        //      by calling onPageTitle()
+        tabLayout.setupWithViewPager(viewPager);
     }
+
 
     @Override
-    public void onBackPressed() {
-        if (back_pressed + 1500 > System.currentTimeMillis()) {
-            super.onBackPressed();
-            // Cancel toast messege when user exit the app
-            mToast.cancel();
+        public void onBackPressed () {
+            if (back_pressed + 1500 > System.currentTimeMillis()) {
+                super.onBackPressed();
+                // Cancel toast messege when user exit the app
+                mToast.cancel();
+            } else {
+                //show toast messege when user press back once
+                mToast = Toast.makeText(MainActivity.this, "Press once again to exit!", Toast.LENGTH_SHORT);
+                // Show toast messege
+                mToast.show();
+            }
+            back_pressed = System.currentTimeMillis();
         }
-        else {
-            //show toast messege when user press back once
-            mToast = Toast.makeText(MainActivity.this, "Press once again to exit!", Toast.LENGTH_SHORT);
-            // Show toast messege
-            mToast.show();
-        }
-        back_pressed = System.currentTimeMillis();
-    }
 
-}
+    }
